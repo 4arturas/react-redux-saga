3c417dfb4c0c664268f978a6be60519d94cf4eed
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-09-08 15:33:24 +0300

    Updates

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,14 +1,13 @@
-import {ADD_TO_CART} from "./constant";
+import { ADD_TO_CART } from "./constant"
 
-export const cartData = ( data=[], action ) =>
-{
-    console.log('reducer called', action );
-    if ( action.type === ADD_TO_CART )
-    {
-        return data;
-    }
-    else
-    {
-        return 'no action called';
+
+
+export const cartData = (data = [], action) => {
+    switch (action.type) {
+        case ADD_TO_CART:
+            console.warn("ADD_TO_CART condition ", action)
+            return [action.data, ...data]
+        default:
+            return []
     }
 }
